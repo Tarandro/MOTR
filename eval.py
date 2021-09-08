da9c37914daab937fd5b6b9ca0fc5bb3b0eb764e
Alexis GASSMANN <alexis.gassmann@expleogroup.com>
2021-09-08 12:10:40 +0200

    r

--- eval.py
+++ eval.py
@@ -168,7 +168,7 @@ class MOTR(object):
 
 
 def load_label(label_path: str, img_size: tuple) -> dict:
-    labels0 = np.loadtxt(label_path, dtype=np.float32).reshape(-1, 6)
+    labels0 = np.loadtxt(label_path, dtype=np.float32, delimiter=',', usecols=range(6)).reshape(-1, 6)
     h, w = img_size
     # Normalized cewh to pixel xyxy format
     labels = labels0.copy()
